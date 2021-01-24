83bd491a6992f3673834af7631248b9494f9a9e8
Miguel Sánchez <miguelsanchez1703@gmail.com>
2021-01-24 16:39:07 +0100

    Retrieve path of the files that make up the epub

--- colorkindle.py
+++ colorkindle.py
@@ -19,3 +19,21 @@ for line in f:
 		lines.append(line.strip())
 
 print (lines[0])
+
+paths = []
+
+for line in lines:
+	paths.append(line.split('"')[1])
+
+print (paths)
+
+paths_def = []
+
+for path in paths:
+	for root, dirs, files in os.walk('.'):
+		for file in files:
+			if file.endswith(path.split('/')[len(path.split('/'))-1]):
+				paths_def.append(os.path.join(root, file))
+
+for p in paths_def:
+	print (p)
